546d8ef231f5701d04b76ce3e8e2452147727c90
sumaiyaQa <168733927+sumaiyaQa@users.noreply.github.com>
2024-05-03 01:07:39 +0400

    Update SprintEntryTest.java

--- src/test/java/com/stir/cscu9t4practical1/SprintEntryTest.java
+++ src/test/java/com/stir/cscu9t4practical1/SprintEntryTest.java
@@ -127,7 +127,7 @@ public class SprintEntryTest {
     public void testGetDistance() {
         System.out.println("getDistance");
         Entry instance = new SprintEntry("Alice", 1, 2, 2003, 0, 16, 7, 300, 4, 2);
-        float expResult = 3.0F;
+        float expResult = 300.0F;
         float result = instance.getDistance();
         assertEquals(expResult, result, 0.0);
     }
@@ -139,8 +139,8 @@ public class SprintEntryTest {
     public void testGetRepetitions() {
         System.out.println("getRepetitions");
         Entry instance = new SprintEntry("Alice", 1, 2, 2003, 0, 16, 7, 300, 4, 2);
-        int expResults = 4;
-        int result = instance.getRepetitions();
+        int expResult = 4;
+        int result = ((SprintEntry) instance).getRepetitions();
         assertEquals(expResult, result);
     }
     
@@ -151,8 +151,8 @@ public class SprintEntryTest {
     public void testGetRecovery() {
         System.out.println("getRepetitions");
         Entry instance = new SprintEntry("Alice", 1, 2, 2003, 0, 16, 7, 300, 4, 2);
-        int expResults = 2;
-        int result = instance.getRecovery();
+        int expResult = 2;
+        int result = ((SprintEntry) instance).getRecovery();
         assertEquals(expResult, result);
     }
 
